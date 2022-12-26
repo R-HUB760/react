ea68d3abbf8c414b600d02edd6e20ad6bdeb6bdd
Abdur Rahim <60249128+R-HUB760@users.noreply.github.com>
2022-12-26 11:56:33 +0600

    Update Products.js

--- src/commonents/Products.js
+++ src/commonents/Products.js
@@ -7,7 +7,7 @@ function Products(props) {
   return (
     <>
       <div className='team rounded p-2 bg-dark'>
-        <img src={pic} alt="Product Photo" />
+        <img src={pic} alt="Product" />
         <h5 className='name'>{name}</h5>
         <h6 className='name text-warning'>Price: {price}</h6>
         <h6 className='title text-warning'>Rating: {rating}</h6>
